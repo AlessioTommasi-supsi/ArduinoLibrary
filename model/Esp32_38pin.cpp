c1e2554e626d4a2e9b1ea58b0d75b313a57f5da6
Alessio Tommasi <alessiotom@live.it>
2024-12-22 15:11:37 +0100

    pin che necessito ma non funzioanno!

--- model/Esp32_38pin.cpp
+++ model/Esp32_38pin.cpp
@@ -20,7 +20,7 @@ void Esp32_38pin::initializePins() {
         Pin defaultPin(0, PinType::UNKNOWN, false, "default pin");
         addPin(defaultPin);
         Serial.println("Pin default aggiunto"); 
-        */
+        
 
         addPin(Pin(-1, PinType::VOUT, false, "3.3V"));
         Serial.println("Pin 3.3V aggiunto");
@@ -109,26 +109,26 @@ void Esp32_38pin::initializePins() {
 
         // Pin GPIO2 - ADC12 
         addPin(Pin(2, PinType::ADC, true, "ADC12"));
-
+        */
         // Pin GPIO9 SD2 SPIHD  HS1 DATA 2
         addPin(Pin(9 , PinType::DIGITAL, true, "Input digital SD2 SPIHD  HS1 DATA 2 ovvero per interfacchiamento scheda sd"));
 
         // Pin GPIO15 - ADC2, HSPI_CS0
-        addPin(Pin(15, PinType::ADC, true, "ADC13, HSPI_CS0  HS2 CMD ovvero per interfacciamento con memoria flash"));
+        //addPin(Pin(15, PinType::ADC, true, "ADC13, HSPI_CS0  HS2 CMD ovvero per interfacciamento con memoria flash"));
 
         // Pin GPIO10 - SD3 SPIWP HS1 DATA 3
         addPin(Pin(10, PinType::DIGITAL, true, "Input digital SD3 SPIWP HS1 DATA 3 ovvero per interfacchiamento scheda sd"));
         
         //Pin GPIO8 - SD2 SPIWP HS1 DATA 1
-        addPin(Pin(8, PinType::DIGITAL, true, "Input digital SD2 SPIWP HS1 DATA 1 ovvero per interfacchiamento scheda sd"));
+        //addPin(Pin(8, PinType::DIGITAL, true, "Input digital SD2 SPIWP HS1 DATA 1 ovvero per interfacchiamento scheda sd"));
 
         // Pin GPIO11 SPICS0 HS1 CMD
         addPin(Pin(11, PinType::DIGITAL, true, "Input digital SPICS0 HS1 CMD ovvero per interfacchiamento scheda sd"));
 
         // Pin GPIO 7 SD0 SPIQ HS1 DATA 0
-        addPin(Pin(7, PinType::DIGITAL, true, "Input digital SD0 SPIQ HS1 DATA 0 ovvero per interfacchiamento scheda sd"));
+        //addPin(Pin(7, PinType::DIGITAL, true, "Input digital SD0 SPIQ HS1 DATA 0 ovvero per interfacchiamento scheda sd"));
 
-        addPin(Pin(-4, PinType::VIN, false, "tensione di alimentazione del dispositivo (5V)"));
+        //addPin(Pin(-4, PinType::VIN, false, "tensione di alimentazione del dispositivo (5V)"));
 
         // Pin GPIO6 SD0 SPID HS1 CLK
         addPin(Pin(6, PinType::DIGITAL, true, "segnale di clock temporizzato"));

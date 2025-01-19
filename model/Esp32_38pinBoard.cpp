49a0b87a89d3d088329c0bad07c42ccf26757521
Alessio Tommasi <alessiotom@live.it>
2025-01-19 14:32:38 +0100

    ho inserito figlio per nuovi pin esp32Board
    FROM W11
    Sketch uses 1291001 bytes (98%) of program
    storage space. Maximum is 1310720 bytes.
    Global variables use 50448 bytes (15%) of dynamic memory, leaving 277232 bytes

---
+++ model/Esp32_38pinBoard.cpp
@@ -0,0 +1,24 @@
+#include "Esp32_38pinBoard.h"
+
+Esp32_38pinBoard::Esp32_38pinBoard() {
+    Serial.println("Costruttore Esp32_38pinBoard");
+    initializePins();
+    Serial.println("Inizializzazione dei pin effettuata!");
+}
+
+void Esp32_38pinBoard::initializePins()
+{
+    //richiama metodo padre
+    Esp32_38pin::initializePins();
+    //utilizza setdi metodo padre per modificare i pin
+    Pin &readedPin = getPin(23);
+
+    if (readedPin.type == PinType::UNKNOWN)
+    {
+        /* il pin non era stato inizializzato, quindi devo aggiungerlo! */
+        Esp32_38pin::addPin(readedPin);
+    }
+    sprintf(readedPin.note, "Esp32_38pinBoard - VSPI MOSI or input digital");
+        
+
+}

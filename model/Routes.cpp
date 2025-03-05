7cf9f90d4721407d32ba912d15a5a624bf152107
AlessioTommasi <126590912+AlessioTommasi-polimi@users.noreply.github.com>
2025-03-05 11:22:28 +0100

    errore e dato da pinoutContent ci mette troppo ed e richiesta troppo pesante!
    vedi da scheda Network di google chrome!

--- model/Routes.cpp
+++ model/Routes.cpp
@@ -38,39 +38,40 @@ void Routes::defineRoutes(AsyncWebServer &server)
     */
     server.on("/pinout", HTTP_GET, [](AsyncWebServerRequest *request){
         String htmlContent =""; 
-        if (request->hasParam("pin")) 
-        {
-            String pinNumber = request->getParam("pin")->value();
-            // arrivo da editPin! dunque devo aggiungere il pin!
-            if (request->hasParam("pinType") && request->hasParam("isInput") && request->hasParam("outputValue") && request->hasParam("pinNote"))
-            {
-                String pinType = request->getParam("pinType")->value();
-                bool isInput = request->getParam("isInput")->value() == "true" ? true : false;
-                uint8_t pinMode = isInput ? INPUT : OUTPUT;
-                float outputValue = request->getParam("outputValue")->value().toFloat();
-                bool goHigh = outputValue > 0;
-                String pinNote = request->getParam("pinNote")->value();
-
-                Pin *pin = &SystemState::getInstance()->pinoutData->getPin(pinNumber.toInt());
-
-                Serial.println("Saving pin " + pinNumber + " with type " + pinType + ", isInput " + String(isInput) + ", pinMode " + String(pinMode) + ", outputValue " + String(outputValue) + ", pinNote " + pinNote);
-                pin->isInput = isInput;
-                pin->setType(pinType);
-                pin->setMode(pinMode);
-                pin->setNote(pinNote.c_str());
-
-                if (!isInput)
-                {
-                    pin->write(goHigh); 
-                }
-            }
-            else
-            {
-                Serial.println("Error: missing parameters");
-            }
-        }
+        
         try
         {
+            if (request->hasParam("pin"))
+            {
+                String pinNumber = request->getParam("pin")->value();
+                // arrivo da editPin! dunque devo aggiungere il pin!
+                if (request->hasParam("pinType") && request->hasParam("isInput") && request->hasParam("outputValue") && request->hasParam("pinNote"))
+                {
+                    String pinType = request->getParam("pinType")->value();
+                    bool isInput = request->getParam("isInput")->value() == "true" ? true : false;
+                    uint8_t pinMode = isInput ? INPUT : OUTPUT;
+                    float outputValue = request->getParam("outputValue")->value().toFloat();
+                    bool goHigh = outputValue > 0;
+                    String pinNote = request->getParam("pinNote")->value();
+
+                    Pin *pin = &SystemState::getInstance()->pinoutData->getPin(pinNumber.toInt());
+
+                    Serial.println("Saving pin " + pinNumber + " with type " + pinType + ", isInput " + String(isInput) + ", pinMode " + String(pinMode) + ", outputValue " + String(outputValue) + ", pinNote " + pinNote);
+                    pin->isInput = isInput;
+                    pin->setType(pinType);
+                    pin->setMode(pinMode);
+                    pin->setNote(pinNote.c_str());
+
+                    if (!isInput)
+                    {
+                        pin->write(goHigh);
+                    }
+                }
+                else
+                {
+                    Serial.println("Error: missing parameters");
+                }
+            }
             htmlContent = Pinout::generateHTML();
             const char *htmlContentPtr = htmlContent.c_str();
             request->send(200, "text/html", htmlContentPtr);

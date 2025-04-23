63bb3b4354fa410160d114ed13dc0aacac3e8504
Alessio Tommasi <alessiotom@live.it>
2025-04-23 14:25:27 +0200

    pagina pinout in locale funzionante

--- view/pinout.cpp
+++ view/pinout.cpp
@@ -19,7 +19,7 @@ String Pinout::pageContent()
     html += "<h1>Pinout</h1>";
 
     // Inserisco un'immagine presa da internet:
-    html += "<img src='https://raw.githubusercontent.com/AlessioTommasi-supsi/iotProject/refs/heads/main/image/ESP-38Pin-pinout.jpg' alt='ESP32 Pinout' style='width: 100%; max-width: 800px;'>";
+    //html += "<img src='https://raw.githubusercontent.com/AlessioTommasi-supsi/iotProject/refs/heads/main/image/ESP-38Pin-pinout.jpg' alt='ESP32 Pinout' style='width: 100%; max-width: 800px;'>";
     html += "<br><br>";
     // Aggiungo una sezione per i dati dei pin
     html += "<div id='pinData'></div>";
@@ -58,7 +58,7 @@ String Pinout::pageContent()
 
 
     //aggiungo grafico da ViewGraph
-    html += viewGraph::generateGraph(SystemState::getInstance()->pinoutData->getPinNumbers(), "getPinValues", "pin");
+    //html += viewGraph::generateGraph(SystemState::getInstance()->pinoutData->getPinNumbers(), "getPinValues", "pin");
 
     return html;
 }

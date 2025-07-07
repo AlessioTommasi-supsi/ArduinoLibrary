5f6f377574b73cb9fcaf90a8ac2ddb2046e24c68
Alessio Tommasi <alessiotom@live.it>
2025-07-07 18:31:59 +0200

    fix duplicate pin

--- model/Esp32_38pinBoard.cpp
+++ model/Esp32_38pinBoard.cpp
@@ -12,26 +12,25 @@ Esp32_38pinBoard::Esp32_38pinBoard() {
  */
 void Esp32_38pinBoard::initializePins()
 {
-    //richiama metodo padre
-    //Esp32_38pin::initializePins();
-    //utilizza set di metodo padre per modificare i pin
+    // NON chiamare Esp32_38pin::initializePins() perché è già chiamato nel costruttore del padre
+    
+    // Modifica i pin specifici per questa board
+    // getPin() restituisce il riferimento al pin esistente se presente
     Pin &pinA = getPin(12);
-
     sprintf(pinA.note, "MUX S0 - A");
     pinA.type = PinType::DIGITAL;
+    pinA.isInput = false; // Imposta come output
     pinA.write(false);
-    addPin(pinA);
 
     Pin &pinB = getPin(13);
     sprintf(pinB.note, "MUX S1 - B");
     pinB.type = PinType::DIGITAL;
+    pinB.isInput = false; // Imposta come output
     pinB.write(false);
-    addPin(pinB);
 
     Pin &pinC = getPin(14);
     sprintf(pinC.note, "MUX S2 - C");
     pinC.type = PinType::DIGITAL;
+    pinC.isInput = false; // Imposta come output
     pinC.write(false);        
-    addPin(pinC);
-
 }
